4707cf4f405f6850ed203853578e97482695a4e7
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-08-28 01:09:38 -0400

    BeginProject

---
+++ src/main/java/com/Glab30956/PathVariable/annotation/PathVariableAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.Glab30956.PathVariable.annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathVariableAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathVariableAnnotationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Glab30956/PathVariable/annotation/PathVariableAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Glab30956.PathVariable.annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathVariableAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
